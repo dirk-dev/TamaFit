b286f424e98430711c7e5015935cfb9437cfe001
Dirk Kiesewetter <dirk.k1@outlook.com>
2019-01-17 10:18:33 -0500

    adjusted hmtl route to account page, added 4 more tamagotchi avatars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,59 +3,58 @@ var db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     if (req.user) {
       res.redirect("/members");
     } else {
-      res.render("index")
-    };
+      res.render("index");
+    }
   });
 
-
-  app.get("/account", function (req, res) {
+  app.get("/account", function(req, res) {
     if (req.user) {
       res.render("account", {
         email: req.user.email,
         id: req.user.id,
         createdAt: req.user.createdAt.substring(0, 10)
-      })
+      });
     } else if (!req.user) {
-      res.render("index")
-    };
+      res.render("account");
+    }
   });
 
-  app.get("/login", function (req, res) {
+  app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     } else {
-      res.render("login_page")
-    };
+      res.render("login_page");
+    }
   });
 
-  app.get("/signup", function (req, res) {
+  app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     } else {
-      res.render("signup")
-    };
+      res.render("signup");
+    }
   });
 
   // if user is logged in, allow access to members page
-  app.get("/members", isAuthenticated, function (req, res) {
+  app.get("/members", isAuthenticated, function(req, res) {
     res.render("members");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
+  app.get("/example/:id", function(req, res) {
     db.Example.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function (dbExample) {
+    }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
