3e94587779769afb3d288296f98164f2e5c16dad
PLS <plsenh@gmail.com>
2019-01-17 19:01:13 -0500

    blog functionality

--- models/workout.js
+++ models/workout.js
@@ -1,18 +1,10 @@
-// models folder
-
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var Workout = sequelize.define("Workout", {
     // Giving the Workout model a name of type STRING
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 20]
-      }
-    }
+    name: DataTypes.STRING
   });
 
-  Workout.associate = function (models) {
+  Workout.associate = function(models) {
     // Associating Workout with Logs
     // When a Workout is deleted, also delete any associated Logs
     Workout.hasMany(models.Log, {
--- routes/workout-apiRoutes.js
+++ routes/workout-apiRoutes.js
@@ -1,77 +1,45 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Requiring our models
 var db = require("../models");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
-
-  // GET route for getting all of the posts
-  app.get("/api/posts", function(req, res) {
-    var query = {};
-    if (req.query.author_id) {
-      query.AuthorId = req.query.author_id;
-    }
+  app.get("/api/workouts", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.Post.findAll({
-      where: query,
-      include: [db.Author]
-    }).then(function(dbPost) {
-      res.json(dbPost);
+    // In this case, just db.Log
+    db.Workout.findAll({
+      include: [db.Log]
+    }).then(function(dbWorkout) {
+      res.json(dbWorkout);
     });
   });
 
-  // Get route for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
+  app.get("/api/workouts/:id", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.Post.findOne({
+    // In this case, just db.Log
+    db.Workout.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Author]
-    }).then(function(dbPost) {
-      res.json(dbPost);
+      include: [db.Log]
+    }).then(function(dbWorkout) {
+      res.json(dbWorkout);
     });
   });
 
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    db.Post.create(req.body).then(function(dbPost) {
-      res.json(dbPost);
+  app.Log("/api/workouts", function(req, res) {
+    db.Workout.create(req.body).then(function(dbWorkout) {
+      res.json(dbWorkout);
     });
   });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post.destroy({
+  app.delete("/api/workouts/:id", function(req, res) {
+    db.Workout.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbPost) {
-      res.json(dbPost);
+    }).then(function(dbWorkout) {
+      res.json(dbWorkout);
     });
   });
 
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post.update(
-      req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      }).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
 };
