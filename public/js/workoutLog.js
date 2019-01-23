752b1eb2a78c4d09bfec079dcbd498ff32ef0a3d
PLS <plsenh@gmail.com>
2019-01-23 16:30:53 -0500

    deleted temp folders

--- public/js/workoutLog.js
+++ public/js/workoutLog.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-  /* global moment */
-
   // logContainer holds all of our logs
   var logContainer = $(".log-container");
   var logCategorySelect = $("#category");
